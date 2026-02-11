75213630415b39bab773ee770c232275cdaf3143
nattanicha uprajong <mota1712@hotmail.com>
2026-02-12 04:08:02 +0700

    feat: view detail

--- web/src/app/components/user-view-modal/user-view-modal.component.ts
+++ web/src/app/components/user-view-modal/user-view-modal.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges ,EventEmitter,Output,SimpleChanges} from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-user-view-modal',
   templateUrl: './user-view-modal.component.html',
   styleUrls: ['./user-view-modal.component.scss']
 })
-export class UserViewModalComponent {
 
+export class UserViewModalComponent implements OnChanges {
+
+  @Input() userId?: number;
+  @Output() close = new EventEmitter<void>();
+
+  userByID?: User;
+
+  constructor(private userService: UserService) {}
+ngOnChanges(changes: SimpleChanges) {
+  if (changes['userId'] && this.userId) {
+    this.userService.getUserById(this.userId).subscribe({
+        next: (res: any) => {
+          this.userByID = res.data;
+        }
+      });
+  }
 }
+
+  cancel() {
+    this.close.emit();
+  }
+}
+
