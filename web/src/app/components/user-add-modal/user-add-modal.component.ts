8c23169b04864c298fe1fa87f8c45d1ca819b528
nattanicha uprajong <mota1712@hotmail.com>
2026-02-12 04:07:33 +0700

    feat:add user

--- web/src/app/components/user-add-modal/user-add-modal.component.ts
+++ web/src/app/components/user-add-modal/user-add-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-user-add-modal',
@@ -7,4 +9,48 @@ import { Component } from '@angular/core';
 })
 export class UserAddModalComponent {
 
+  @Output() userCreated = new EventEmitter<void>();
+  @Output() close = new EventEmitter<void>();
+
+  newUser: User = {
+    first_name_th: '',
+    last_name_th: '',
+    birth_date: '',
+    addresses: ''
+  };
+
+  constructor(private userService: UserService) {}
+
+  submit() {
+    this.userService.createUser(this.newUser).subscribe({
+      next: () => {
+        this.userCreated.emit();   // บอก parent ว่าสร้างเสร็จแล้ว
+        this.reset();
+      }
+    });
+  }
+
+  cancel() {
+    this.reset();
+    this.close.emit();   // บอก parent ให้ปิด modal
+  }
+
+  reset() {
+    this.newUser = {
+      first_name_th: '',
+      last_name_th: '',
+      birth_date: '',
+      addresses: ''
+    };
+  }
+
+  get calculateAge(): number | null {
+    if (!this.newUser.birth_date) return null;
+
+    const todayYear = new Date().getFullYear();
+    const birthYear = new Date(this.newUser.birth_date).getFullYear();
+
+    const age = todayYear - birthYear;
+    return age <= 0 ? null : age;
+  }
 }
